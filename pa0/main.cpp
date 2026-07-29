a0468fa98b814581c7e0e543fc139d6f18e96984
kas030 <kas_1120@163.com>
2026-07-29 11:01:41 +0800

    finish pa0

--- pa0/main.cpp
+++ pa0/main.cpp
@@ -1,48 +1,22 @@
-#include<cmath>
-#include<Eigen/Core>
-#include<Eigen/Dense>
-#include<iostream>
+#include <Eigen/Core>
+#include <cmath>
+#include <iostream>
 
-int main(){
+int main() {
+    Eigen::Vector3d P;
+    P << 2, 1, 1;
 
-    // Basic Example of cpp
-    std::cout << "Example of cpp \n";
-    float a = 1.0, b = 2.0;
-    std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
-    std::cout << std::sqrt(b) << std::endl;
-    std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+    const double sqrt2_2 = sqrt(2) / 2;
 
-    // Example of vector
-    std::cout << "Example of vector \n";
-    // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
-    // vector output
-    std::cout << "Example of output \n";
-    std::cout << v << std::endl;
-    // vector add
-    std::cout << "Example of add \n";
-    std::cout << v + w << std::endl;
-    // vector scalar multiply
-    std::cout << "Example of scalar multiply \n";
-    std::cout << v * 3.0f << std::endl;
-    std::cout << 2.0f * v << std::endl;
+    Eigen::Matrix3d R45;
+    R45 << sqrt2_2, -sqrt2_2, 0, sqrt2_2, sqrt2_2, 0, 0, 0, 1;
 
-    // Example of matrix
-    std::cout << "Example of matrix \n";
-    // matrix definition
-    Eigen::Matrix3f i,j;
-    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
-    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
-    // matrix output
-    std::cout << "Example of output \n";
-    std::cout << i << std::endl;
-    // matrix add i + j
-    // matrix scalar multiply i * 2.0
-    // matrix multiply i * j
-    // matrix multiply vector i * v
+    Eigen::Matrix3d T12;
+    T12 << 1, 0, 1, 0, 1, 2, 0, 0, 1;
+
+    auto P_prime = T12 * R45 * P;
+
+    std::cout << P_prime << std::endl;
 
     return 0;
 }
